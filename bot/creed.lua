163a64b95280f2ed9805fbd3c15c77bbe654cb8f
MRMV2000 <mohammadrezavahed@gmail.com>
2016-02-16 18:29:15 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -220,7 +220,7 @@ function create_config( )
     "lock_link",
     "all"
     },
-    sudo_users = {152590594,175579471},--Sudo users
+    sudo_users = {152590594,180873939,180878160},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
